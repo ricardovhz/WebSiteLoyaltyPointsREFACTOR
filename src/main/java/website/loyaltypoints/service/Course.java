d7cae440e2e853f92c2053145b73e8d84705eda5
Rafael Melo <mellotario@gmail.com>
2021-11-01 11:06:08 -0300

    create table

--- src/main/java/website/loyaltypoints/service/Course.java
+++ src/main/java/website/loyaltypoints/service/Course.java
@@ -1,8 +1,17 @@
 package website.loyaltypoints.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class Course {
 
     String courseId;
@@ -39,6 +48,23 @@ public class Course {
         Reservation novaReservaVaga = new Reservation(nomeEstudante, emailEstudante);
         reservations.add(novaReservaVaga);
         numberOfSeats--;
+
+        String DB_URL = "jdbc:h2:mem:testdbs://localhost:8080/h2";
+        String USER = "sa";
+        String PASS = "";
+
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Execute a query
+            System.out.println("Inserting records into the table...");
+            String sql = String.format("INSERT INTO TBL_RESERVATIONS(studentName,studentEmail) VALUES ('%s', '%s')", nomeEstudante, emailEstudante);
+            stmt.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return novaReservaVaga.id;
     }
 
